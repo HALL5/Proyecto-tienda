483cce52b48bb7cc53f016753ce4721df2473356
HALL5 <125421076+HALL5@users.noreply.github.com>
2023-02-14 20:55:34 -0600

    Update Source.cpp

--- Avance1/Source.cpp
+++ Avance1/Source.cpp
@@ -1,20 +1,28 @@
 
 
 #include <iostream>
+
+//para texto de color amarillo
 #include <windows.h>
+
+
+
 using namespace std;
 int main()
 {
+
+	//VARIABLES
 	int opcion,noItem, noArticulo;
 	char descripcion[500], genero[25], clasificacion[12], consola[12];
 	float precioUni, impuestos, precioTot;
 
+	
+	//MENU DE OPCIONES
 	cout << "           " << endl;
 	system("Color 06");
 	cout << "**MENU DE OPCIONES**" << endl;
-
 	cout << "           " << endl;
-	cout << "¿Que desea hacer?. Ingrese el numero de la opcion que quiere realizar." << endl;
+	cout << "Ingrese el numero de la opcion que quiere realizar." << endl;
 	cout << "           " << endl;
 	cout << "1. Alta de Articulo." << endl;
 	cout << "2.- Modificacion de Articulo." << endl;
@@ -22,15 +30,17 @@ int main()
 	cout << "4.- Lista de Articulos." << endl;
 	cout << "5.- Limpiar Pantalla." << endl;
 	cout << "6.- Salir." << endl;
-	
 	cin >> opcion;
 
+
+	//SWITCH PARA LAS OPCIONES DEL MENU
 	switch (opcion)
 	{
-	case 1:
+	case 1: //ALTA DE ARTICULOS
+
 		cout << "Ingreae el no de item." << endl;
 		cin >> noItem;
-		cout << "Ingrese el numero de articulo. " << endl;
+		cout << "Ingrese el numero de articulos. " << endl;
 		cin >> noArticulo;
 		cout << "Ingrese la descripcion del articulo." << endl;
 		cin >> descripcion;
@@ -38,33 +48,36 @@ int main()
 		cin >> genero;
 		cout << "Ingrese la clasificacion." << endl;
 		cin >> clasificacion;
-		cout << "Ingrese para que consola." << endl;
+		cout << "Ingrese la consola." << endl;
 		cin >> consola;
 		cout << "Ingrese el precio unitario." << endl;
 		cin >> precioUni;
-		impuestos = (precioUni * 16) / 100;
+		impuestos = (precioUni * 16) / 100; //operaciones para sacar el precio con impuestos
 		precioTot = precioUni + impuestos;
-
+		
 		return main();
+		break;
+
+
+	case 2: //MODIFICACION DE ARTICULOS
 
 		break;
 
-	case 2:
-
+	case 3: //BAJA DE ARTICULOS
 		break;
 
-	case 3:
+
+	case 4://LISTA ARTICULOS
 		break;
 
-	case 4:
-		break;
 
-	case 5:
+	case 5: //LIMPIAR PANTALLA
 		system("cls");
-		return main();
+		return main ();
 		break;
 
-	case 6:
+
+	case 6: //SALIR
 
 		exit(1);
 		break;
